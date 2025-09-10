b89beaec9a3c2a657df0888b0754ace4de68a4b0
henrique.montenegro <henrique.montenegro@slcagricola.com.br>
2025-09-10 15:18:05 -0300

    Parametros e tipos de retorno

--- lib/funcoes_basicas.dart
+++ lib/funcoes_basicas.dart
@@ -1,5 +1,5 @@
 void main() {
-  var funcaoRecuperarIdade = recuperarIdade;
+  Function funcaoRecuperarIdade = recuperarIdade;
 
   funcaoRecuperarIdade();
 }
---
+++ lib/parametros_tipo_retorno.dart
@@ -0,0 +1,62 @@
+void main() {
+//   soma(1, 2);
+
+//   exibirMensagem('Olá, mundo!');
+
+//   print(saudacaoOpcional('Henrique'));
+//   print(saudacaoOpcional('Henrique', 'Buenas'));
+
+//   print(saudacao());
+//   print(saudacao('Henrique'));
+//   print(saudacao('Henrique', 'Buenas'));
+
+//   print(formatarEndereco(rua: 'Rua A', cidade: 'Cidade B'));
+//   print(formatarEndereco(rua: 'Rua A'));
+//   print(formatarEndereco(cidade: 'Cidade B'));
+//   print(formatarEndereco());
+
+  print(formatarEnderecoObrigatorio(rua: 'Rua A', cidade: 'Cidade B'));
+}
+
+// // Records
+// (int, int) funcaoQualquer() {
+//     return (1, 2);
+// }
+
+int? funcaoQualquer2() {
+  return 1;
+}
+
+// - PARÂMETROS
+// Obrigatórios
+int soma(int valorA, int valorB) {
+  return valorA + valorB;
+}
+
+void exibirMensagem(String mensagem) {
+  print(mensagem);
+}
+
+// Opcionais
+String saudacaoOpcional(String nome, [String? saudacao]) {
+  saudacao = saudacao ?? 'Olá';
+  return '$saudacao, $nome!';
+}
+
+String saudacao([String? nome, String? saudacao]) {
+  nome = nome ?? 'Anônimo';
+  saudacao = saudacao ?? 'Olá';
+  return '$saudacao, $nome!';
+}
+
+// Nomeados Opcionais
+String formatarEndereco({String? rua, String? cidade}) {
+  rua = rua ?? 'Rua não informada';
+  cidade = cidade ?? 'Cidade não informada';
+  return '$rua, $cidade';
+}
+
+// Nomeados Obrigatórios
+String formatarEnderecoObrigatorio({required String rua, required String cidade}) {
+  return '$rua, $cidade';
+}
